b35843e783be51c49ee491390b3b733f07e7be14
afsheenikhlaq732-code <afsheenikhlaq732@gmail.com>
2026-09-10 13:53:06 +0500

    Add files via upload

---
+++ experiment2task2.py
@@ -0,0 +1,12 @@
+john=3
+mary=5
+adam=6
+print("john,mary,adam")
+totalapples=john+mary+adam
+print("the totalapples",totalapples)
+if(totalapples<10):
+    print("totalapple is greater")
+elif(totalapples>10):
+    print("totalapple is smaller")
+else:
+    print("totalapple is equal")        
