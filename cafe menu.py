516e95cdc34f219e12dd94c42dbaa67b448c83ae
afsheenikhlaq732-code <afsheenikhlaq732@gmail.com>
2026-09-17 23:00:35 +0500

    Add files via upload

---
+++ cafe menu.py
@@ -0,0 +1,10 @@
+print("1.loadedfries,2.pizza,3.burger")
+cafemenu=int(input("enter the num of choice"))
+if(cafemenu==1):
+    print("loadedfries is ready")
+elif(cafemenu==2):
+    print("pizza is ready")    
+elif(cafemenu==3):
+    print("burger is ready")
+else:
+    print("order is not ready")        
